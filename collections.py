ba5b9143f5ae61cf6702e8916238c4cbc72995d2
ArnavKarwa07 <arnavkarwa07@gmail.com>
2025-01-10 15:37:27 +0530

    #1 - added collections in python

---
+++ collections.py
@@ -0,0 +1,22 @@
+# Lists
+fruits = ['apple', 'banana', 'orange']
+print(fruits)
+print(fruits[0])
+
+# Tuples
+afruits = ('apple', 'banana', 'orange')
+print(afruits)
+print(afruits[0])
+
+# Sets
+numbers = {1, 2, 3}
+print(numbers)
+
+#Dictionaries
+name = {
+    "first_name" : "Arnav",
+    "last_name" : "Karwa"
+}
+
+print(name)
+print(name["first_name"])
